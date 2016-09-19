19ac869a7e81ded1df3a6bec3660f32af8617d73
vogosvagen <mhd.bassel@gmail.com>
2016-09-19 20:33:22 +0200

    First

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('CONSUMER_KEY', 'JljucZPl8voJPgE3MuPYK4Uwr'); // add your app consumer key between single quotes
+define('CONSUMER_SECRET', 'gN4WLBHpb4kuPanpw4XqmMFy6jrQ4DdzZt9UT0KJ3PmuwPCVCm'); // add your app consumer secret key between single quotes
+define('OAUTH_CALLBACK', 'https://OfferLeb.com/callback.php'); // your app callback URL
+
+
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require 'autoload.php';
+require 'config.php';
+
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+if (!isset($_SESSION['access_token'])) {
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
+	$request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => OAUTH_CALLBACK));
+	$_SESSION['oauth_token'] = $request_token['oauth_token'];
+	$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+	$url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
+	echo $url;
+} else {
+	$access_token = $_SESSION['access_token'];
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+	$user = $connection->get("account/verify_credentials");
+	echo $user->screen_name;
+}
+?>
